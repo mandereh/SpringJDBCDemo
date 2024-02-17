6143233a5dfafbd78bc0b95bddef8ed150873464
Daerego Braide <phronesis4xt@gmail.com>
2024-02-17 23:00:42 +0100

    first and only commit SpringJDBCDemo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'Daerego Braide','Blockchain');
+insert into alien (id, name, tech) values (102, 'Iyowuna Somiari','AI');
+insert into alien (id, name, tech) values (103, 'Akonte Braide','IOT');
